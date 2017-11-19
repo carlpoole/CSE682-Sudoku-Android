a6bc1c25b4b047bf4aafdbcf138ad4da3ef1b96b
Carl Poole <mail@carlpoole.com>
2017-11-19 13:57:01 -0500

    Initial Commit

---
+++ app/src/main/java/codes/carl/sudoku/Network/API.java
@@ -0,0 +1,11 @@
+package codes.carl.sudoku.Network;
+
+/**
+ * Created by cma on 11/19/17.
+ */
+
+public interface API {
+
+
+
+}
---
+++ app/src/main/java/codes/carl/sudoku/Network/Client.java
@@ -0,0 +1,12 @@
+package codes.carl.sudoku.Network;
+
+/**
+ * Created by cma on 11/19/17.
+ */
+
+public class Client {
+
+
+
+
+}
